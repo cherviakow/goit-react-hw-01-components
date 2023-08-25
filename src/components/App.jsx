6bfb72a8e3f2485ba8fb8e2147651252f93031a6
Ihor Cherviakov <118768294+cherviakow@users.noreply.github.com>
2023-08-25 10:34:26 -0400

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
-export const App = () => {
+import user from 'data/user';
+import Profile from "./Profile/Profile";
+import data from './data/data';
+
+export default function App(){
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+      avatar={user.avatar}
+      name={user.name}
+      tag={user.tag}
+      location={user.location}
+      />
     </div>
   );
-};
+}
+
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
